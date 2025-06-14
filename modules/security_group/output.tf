0addca5752224a40496f2bf8881caf2f574d2cc0
charanjit-dev <charanjit.singh@outlook.in>
2025-06-15 04:50:58 +0530

    security group output updated

--- modules/security_group/output.tf
+++ modules/security_group/output.tf
@@ -1,4 +1,4 @@
 output "web_sg" {
   # Mapping security group keys to their IDs for easier reference and usage
-  value = { for k, v in aws_security_group.web_sg : k => v.id }
+  value  = { for k, v in aws_security_group.web_sg : k => v.id }
 }
